a0b9483ea16f8cc63c1e994f4d8b17a26ee60f86
Fekete Jenő <feketej@kkszki.hu>
2025-12-04 17:12:15 +0100

    feladat4 is kész.

---
+++ feladat4.py
@@ -0,0 +1,12 @@
+#4. feladat – Számjegyek összege. Kérj be egy számot, majd számold ki a számjegyeinek összegét.
+
+n = (input("Kérek egy számot: "))
+
+ossz = 0
+szamJegy = 0
+
+for i in range(len(n)):
+    ossz = ossz + int(n[i])
+
+print(f"A számjegyek összege: {ossz}")
+
